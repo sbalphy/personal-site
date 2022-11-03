83a0dc784d8f3eb9ccf04fbe9f2becf5fc075790
sunny <joelsoaresmoreira@gmail.com>
2022-11-03 11:52:07 -0300

    Finalização do utilitário para criar páginas.

--- utils/createpage.py
+++ utils/createpage.py
@@ -8,28 +8,55 @@ from anytree.resolver import Resolver
 import sys
 from tabulate import tabulate
 from itertools import zip_longest
-from html import escape
+from jinja2 import Environment, FileSystemLoader
+
+def addnode(resolve, root, path):
+    path_parent, name_child = path.rsplit("/", 1)
+    parent = resolve.get(root, path_parent)
+    child = Node(name_child, parent=parent)
+    return
+
+def removenode(resolve, root, path):
+    target = resolve.get(root, path)
+    target.parent = None
+    return
+
+def createpage(resolve, root, path):
+    environment = Environment(loader=FileSystemLoader("templates/"), trim_blocks=True, lstrip_blocks=True)
+    template = environment.get_template("template.html")
+    target = resolve.get(root, path)
+    pathlist = path.split("/")
+    filename = f"{target.name}.html"
+    table = [[child.name for child in n.children] for n in target.path][1:]
+    table = transpose(table)
+    content = template.render(
+        name = target.name,
+        path = path,
+        table = table,
+        pathlist = pathlist,
+        children = [child.name for child in target.children]
+    )
+    with open(filename, mode="w", encoding="utf-8") as page:
+        page.write(content)
+        print(f"Page {filename} created.")
+    return
+
+def transpose(table):
+    return [list(x) for x in zip_longest(*table)]
 
 def main():
     command = sys.argv[1]
     path = sys.argv[2]
     with open("./files.json", "r+") as f:
         index = JsonImporter().read(f)
-        r = Resolver()
+        resolve = Resolver()
         fakeroot = Node("fakeroot", children=[index])
         if command == "add":
-            path_parent, name_child = path.rsplit("/", 1)
-            parent = r.get(fakeroot, path_parent)
-            child = Node(name_child, parent=parent)
+            addnode(resolve, fakeroot, path)
         elif command == "remove":
-            target = r.get(fakeroot, path)
-            target.parent = None
-        elif command == "table":
-            target = r.get(fakeroot, path)
-            table = [[f"<a href=\"./{child.name}.html\">{child.name}</a>" for child in n.children] for n in target.path][1:]
-            table = [list(x) for x in zip_longest(*table)]
-            print("/".join([n.name for n in target.path[1:]]))
-            print(tabulate(table, tablefmt="unsafehtml", colalign=None))
+            removenode(resolve, fakeroot, path)
+        elif command == "create":
+            createpage(resolve, fakeroot, path)
         else:
             print(f"Unidentified command {command}. Nothing has been updated.")
         fakeroot.children=[]
